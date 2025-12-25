cd5f33fb37faa4d84f0f0135ba2937b84841df35
phuongnam8290 <phuongnam8290@gmail.com>
2025-12-25 13:12:13 +0700

    Handle Invalid Command

--- src/main.cs
+++ src/main.cs
@@ -2,7 +2,10 @@ class Program
 {
     static void Main()
     {
-        // TODO: Uncomment the code below to pass the first stage
         Console.Write("$ ");
+        
+        // Wait for user input
+        String command = Console.ReadLine();
+        Console.WriteLine($"{command}: command not found");
     }
 }
