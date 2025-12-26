1c0e503491d9057adc0df39db9f3958d6522a3d9
phuongnam8290 <phuongnam8290@gmail.com>
2025-12-26 01:51:52 +0000

    feat: Implement type Command

--- src/main.cs
+++ src/main.cs
@@ -12,17 +12,29 @@ class Program
             
             // Normalize user input: split by whitespace, remove empty entries.
             string[] tokens = input.Split((char[]?)null, StringSplitOptions.RemoveEmptyEntries);
-            tokens = tokens.Length == 0 ? [""] : tokens; // Ensure a non-empty token list for command handling.
-            string command = tokens[0];
+            
+            (string command, string[] arguments) = tokens switch {
+                null or [] => ("", Array.Empty<String>()),
+                [var cmd] => (cmd, Array.Empty<String>()),
+                [var cmd, .. var args] => (cmd, args)
+            };
             
             if (CompareCommand(command, "exit")) { break; }
 
             if (CompareCommand(command, "echo")) {
-                // Print the input message. Index bounds checks are unnecessary because the tokens array is guaranteed to contain at least
-                // one element; if only one token exists, there is nothing to print.
-                String result = String.Join(" ", tokens, 1, tokens.Length - 1);
+                // Print the input message. If there are no arguments, the result is an empty string ("").
+                String result = String.Join(" ", arguments);
                 Console.WriteLine(result);
                 
+                continue;
+            }
+
+            if (CompareCommand(command, "type")) {
+                Console.WriteLine(
+                    Enum.TryParse<Command>(arguments[0], true, out _)
+                    ? $"{arguments[0]} is a shell builtin"
+                    : $"{arguments[0]}: not found");
+
                 continue;
             }
             
@@ -30,6 +42,8 @@ class Program
         }
     }
 
+    public enum Command { Exit, Echo, Type}
+    
     private static bool CompareCommand(string token, string command) {
         return token.Trim().Equals(command, StringComparison.OrdinalIgnoreCase);
     }
