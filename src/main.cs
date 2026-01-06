446d7ce1c93f2636a4e61fb5f7a43ebf38143f37
phuongnam8290 <phuongnam8290@gmail.com>
2026-01-06 16:14:58 +0000

    feat: Locate Executable Files
    Locate executable files when using type command in both Windows and Linux environments.

--- src/main.cs
+++ src/main.cs
@@ -1,4 +1,3 @@
-using System.Text;
 using codecrafters_shell.Commands.BuiltIn;
 
 class Program
@@ -57,9 +56,71 @@ class Program
             return;
         }
         
+        if (Enum.TryParse<BuiltInCommand>(arguments[0], true, out _)) {
+            Console.WriteLine($"{arguments[0]} is a shell builtin");
+            return;
+        }
+        
+        if (IsExecutableFileExist(arguments[0], out string pathIfExists)) {
+            Console.WriteLine($"{arguments[0]} is {pathIfExists}");
+            return;
+        }
+        
         Console.WriteLine(
             Enum.TryParse<BuiltInCommand>(arguments[0], true, out _)
                 ? $"{arguments[0]} is a shell builtin"
                 : $"{arguments[0]}: not found");
     }
-}
+    
+    private static bool IsExecutableFileExist(string fileName, out string pathIfExists) {
+        string? path = Environment.GetEnvironmentVariable("PATH");
+        string[] paths = (path ?? string.Empty).Split(Path.PathSeparator, StringSplitOptions.RemoveEmptyEntries);
+        
+        if (OperatingSystem.IsWindows()) {
+            string[] pathext = (Environment.GetEnvironmentVariable("PATHEXT") ?? string.Empty)
+                .Split(Path.PathSeparator, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string filePath in paths) {
+                if (!Directory.Exists(filePath)) { continue; }
+
+                string[] files = Directory.GetFiles(filePath);
+                bool exists = (
+                    from file in files
+                    let name = Path.GetFileNameWithoutExtension(file)
+                    let ext = Path.GetExtension(file)
+                    where name.Equals(fileName, StringComparison.OrdinalIgnoreCase) &&
+                          pathext.Contains(ext, StringComparer.OrdinalIgnoreCase)
+                    select file)
+                    .Any();
+
+                if (exists) {
+                    pathIfExists = Path.Combine(filePath, fileName);
+                    return true;
+                }
+            }
+            
+        } else if (OperatingSystem.IsLinux()) {
+            foreach (string filePath in paths) {
+                if (!Directory.Exists(filePath)) { continue; }
+                
+                string[] files = Directory.GetFiles(filePath);
+                bool exists = (from file in files
+                    let name = Path.GetFileName(file)
+                    let fileMode = File.GetUnixFileMode(file)
+                    where name.Equals(fileName, StringComparison.CurrentCultureIgnoreCase) &&
+                          fileMode.HasFlag(UnixFileMode.UserExecute | 
+                                           UnixFileMode.GroupExecute | 
+                                           UnixFileMode.OtherExecute)
+                    select file).Any();
+                
+                if (exists) {
+                    pathIfExists = Path.Combine(filePath, fileName);
+                    return true;
+                }
+            }
+        }
+
+        pathIfExists = String.Empty;
+        return false;
+    }
+} 
